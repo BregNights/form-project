e8c7e6a088e49a39da09ef93a72cb96766228cc5
BregNights <daniel_baumann567@hotmail.com>
2025-02-08 18:27:57 -0300

    refactoring

--- engine.js
+++ engine.js
@@ -38,17 +38,6 @@ document.querySelectorAll('input[name="menstruacao"]').forEach(radio => {
   radio.addEventListener("change", () => faseMenstrauacaoSelecionado = radio.value)
 })
 
-function CreateBtnRemover(local) {
-  const CreateTDAcoes = document.createElement('td')
-  const createBtnRemover = document.createElement('button')
-  createBtnRemover.classList.add('btnRemover')
-  createBtnRemover.innerHTML = '❌'
-
-  CreateTDAcoes.appendChild(createBtnRemover)
-
-  local.appendChild(CreateTDAcoes)
-}
-
 document.querySelector('table').addEventListener('click', function (event) {
   if (event.target.classList.contains('btnRemover')) {
     Remover(event.target.closest('tr'))
@@ -69,49 +58,56 @@ async function Remover(linha) {
   }
 }
 
-function CreateTD(local, text) {
-  const CreateTD = document.createElement('td')
-  CreateTD.innerText = text
-  local.appendChild(CreateTD)
-}
-
 function dataAtual() {
   const dataAtual = new Date().toLocaleDateString("pt-BR")
   return dataAtual
 }
 
+let datainput = ''
+const date = document.querySelector('#date')
+date.addEventListener('change', function () {
+  const date = new Date(this.value + 'T00:00:00')
+  datainput = date.toLocaleDateString('pt-BR').replace(/\//g, "-")
+  console.log(datainput)
+})
+
 function Gerar() {
   const pesoValor = parseFloat(document.getElementById('peso').value)
-    const tBody = document.querySelector('tbody.res')
+  const tBody = document.querySelector('tbody.res')
 
-    const createTR = document.createElement('tr')
-    createTR.classList.add('informacoes')
-    tBody.appendChild(createTR)
+  
 
-    CreateTD(createTR, dataAtual())
-    CreateTD(createTR, almocoSelecionado)
-    CreateTD(createTR, jantarSelecionado)
-    CreateTD(createTR, besteiraSelecionado)
-    CreateTD(createTR, faseMenstrauacaoSelecionado)
-    CreateTD(createTR, `${pesoValor} Kg`)
-    CreateBtnRemover(createTR)
+  
+    
+    tBody.innerHTML += `<tr class="informacoes">
+                          <td>${datainput}</td>
+                          <td>${almocoSelecionado}</td>
+                          <td>${jantarSelecionado}</td>
+                          <td>${besteiraSelecionado}</td>
+                          <td>${faseMenstrauacaoSelecionado}</td>
+                          <td>${pesoValor} Kg</td>
+                          <td class="btnAcoes">
+                            <button class="btnEditar">✏️</button>
+                            <button class="btnRemover">❌</button>
+                          </td>
+                      </tr>`
 }
 
 async function salvarDados() {
-  const dataAtual = new Date().toLocaleDateString("pt-BR").replace(/\//g, "-")
+  // const dataAtual = new Date().toLocaleDateString("pt-BR").replace(/\//g, "-")
   const pesoValor = parseFloat(document.getElementById('peso').value)
 
   const dados = {
-    data: dataAtual,
+    data: datainput,
     almoco: almocoSelecionado,
     jantar: jantarSelecionado,
     besteira: besteiraSelecionado,
     menstruacao: faseMenstrauacaoSelecionado,
-    peso: `${pesoValor} Kg`
+    peso: pesoValor,
   }
 
   try {
-    await setDoc(doc(collection(db, "registros"), dataAtual), dados)
+    await setDoc(doc(collection(db, "registros"), datainput), dados)
     alert("Dados salvos com sucesso!")
   } catch (error) {
     console.error("Erro ao salvar os dados:", error)
@@ -128,18 +124,18 @@ async function carregarDados() {
     querySnapshot.forEach((doc) => {
       const dados = doc.data()
 
-      const createTR = document.createElement('tr')
-      createTR.classList.add('informacoes')
-
-      CreateTD(createTR, dados.data.replace(/-/g, "/"))
-      CreateTD(createTR, dados.almoco)
-      CreateTD(createTR, dados.jantar)
-      CreateTD(createTR, dados.besteira)
-      CreateTD(createTR, dados.menstruacao)
-      CreateTD(createTR, dados.peso)
-      CreateBtnRemover(createTR)
-
-      tBody.appendChild(createTR)
+      tBody.innerHTML += `<tr class="informacoes">
+                          <td>${dados.data.replace(/-/g, "/")}</td>
+                          <td>${dados.almoco}</td>
+                          <td>${dados.jantar}</td>
+                          <td>${dados.besteira}</td>
+                          <td>${dados.menstruacao}</td>
+                          <td>${dados.peso} Kg</td>
+                          <td class="btnAcoes">
+                            <button class="btnEditar">✏️</button>
+                            <button class="btnRemover">❌</button>
+                          </td>
+                      </tr>`
     })
 
   } catch (error) {
@@ -176,6 +172,7 @@ document.getElementById("btnGerar").addEventListener("click", () => {
 
   Gerar()
   salvarDados()
+  carregarDados()
   document.getElementById('peso').value = ''
   document.getElementById('peso').focus()
 })
